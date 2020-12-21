6e7cbd1598606861edc6491e1c5f8601f369ffdc
gwlodawiec <gwlodawiec@gmail.com>
2020-12-21 11:37:41 +0100

    Remove Spring kafka stream config

--- src/main/java/com/example/kafka/ApacheKafkaConsumerApplication.java
+++ src/main/java/com/example/kafka/ApacheKafkaConsumerApplication.java
@@ -1,9 +1,12 @@
 package com.example.kafka;
 
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
@@ -13,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 import com.example.kafka.models.Transaction;
@@ -26,12 +30,24 @@ public class ApacheKafkaConsumerApplication {
 	
 	private static String inputTopic;
 	private static String outputTopic;
+	private static String bootstrapAddress;
 	
-	@Value("${spring.kafka.bootstrap-servers}")
-	private String bootstrapAddress;
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(ApacheKafkaConsumerApplication.class, args);
+		
+		Properties config = new Properties();
+		config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		config.put(StreamsConfig.APPLICATION_ID_CONFIG, "other-group");
+		config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String()
+				.getClass());
+		config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class);
+		config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.kafka.models");
+		
+		Topology topology = getTransactionsTopology();
+
+        KafkaStreams ks = new KafkaStreams(topology, config);
+        ks.start();
+
 	}
 	
 	public static Topology getTransactionsTopology() {
@@ -59,6 +75,11 @@ public class ApacheKafkaConsumerApplication {
 	@Value("${default.kafka.output.topic.name}")
 	public void setOutputTopicName(String name) {
 		ApacheKafkaConsumerApplication.outputTopic = name;
+	}
+	
+	@Value("${spring.kafka.bootstrap-servers}")
+	public void setBootstrapServers(String name) {
+		ApacheKafkaConsumerApplication.bootstrapAddress = name;
 	}
 	
 }
